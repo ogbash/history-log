26d1523f73e633e909c720393103334464058fb0
Oleg Batrashev <ogbash@gmail.com>
2012-12-16 20:42:40 +0200

    Move search sources by title.

--- site/rpc/sourcecheck.php
+++ site/rpc/sourcecheck.php
@@ -1,8 +1,8 @@
 <?php
 
-require(dirname(__FILE__) . "/xml.php");
+require_once(dirname(__FILE__) . "/xml.php");
+require_once("../lib/source.php");
 
-function esc($reqstr) { return mysql_real_escape_string($reqstr); }
 function parse_request($xmldoc) {
   $a = array();
   $a['type'] = $xmldoc->documentElement->getAttribute('type');
@@ -15,16 +15,8 @@ $req = parse_request($xml);
 
 // connect to DB and search source
 
-$db = mysql_connect('localhost','root','ares') or die("Connection failed!!!");
-mysql_select_db('historylog') or die ("Databse selection failed!!!");
-mysql_set_charset('utf8',$db); 
-
-$q = mysql_query('SELECT * FROM sources WHERE title LIKE "'.esc($req['title']).'%"') or die("mysql query failed");
-
-$result = array();
-while($source = mysql_fetch_assoc($q)) {
-  array_push($result, $source);
-}
+require(dirname(__FILE__) . "/../database_connect.php");
+$result = searchSourcesByTitle($req['title']);
 mysql_close($db) or die("Closing db failed");
 
 // create and send response
